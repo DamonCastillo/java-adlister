e4f47094da08ae2a443fdc23a14ffc6e6c05d018
Damon Castillo <damono7.castillo@gmail.com>
2022-07-20 09:22:40 -0500

    completed sessions-and-cookies

--- src/main/java/LogoutServlet.java
+++ src/main/java/LogoutServlet.java
@@ -6,15 +6,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
-public class ViewProfileServlet extends HttpServlet {
+
+@WebServlet(name ="logoutServlet" ,urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
+        session.invalidate();
+        response.sendRedirect("/logout");
 
-        if (session.getAttribute("user") == null) {
-            response.sendRedirect("/login");
-        } else {
-            request.getRequestDispatcher("/profile.jsp").forward(request, response);
-        }
     }
 }
